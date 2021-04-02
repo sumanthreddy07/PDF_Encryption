5320d2e0f150edef06dfe33080c47f8cbd67d7a4
sumanthreddy07 <kota.sumanthreddy@gmail.com>
2021-04-02 10:46:28 +0530

    add files via upload

---
+++ decrypt.py
@@ -0,0 +1,50 @@
+#import Section
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import time
+import sys
+import threading
+import itertools
+
+#File name and password
+filename = input("Enter File Name: ")
+
+if filename[-3:]!="pdf":
+    filename += ".pdf"
+
+if not os.path.isfile(filename):
+    print("Invalid Filename / File Doesn't Exist.")
+    exit()
+
+password = input("Enter password: ")
+
+oldfile = PdfFileReader(filename)
+output = PdfFileWriter()
+
+if oldfile.isEncrypted:
+    oldfile.decrypt(password)
+
+# 'Loading' Animation 
+    done = False
+    def animate():
+        for c in itertools.cycle(['|', '/', '-', '\\']):
+            if done:
+                break
+            sys.stdout.write('\rLoading ' + c)
+            sys.stdout.flush()
+            time.sleep(0.1)
+        sys.stdout.write('\rDone! File is now decrypted!\n')
+
+    t = threading.Thread(target=animate)
+    t.start()
+
+    #Read Pages and Encrypt
+    for i in range(oldfile.numPages):
+        page = oldfile.getPage(i)
+        output.addPage(page)
+
+    with open("decrypt.pdf","wb") as newfile:
+        output.write(newfile)
+    done =True
+else:
+    print("File is already decrypted!")
